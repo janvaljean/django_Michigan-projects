d866848225f9752909736ad2a6855e2d12f5a319
janvaljean <sfkrtld@gmail.com>
2022-11-28 12:11:54 +0100

    hello,goodbye,main templates created

--- django1_demo/django1_demo/base/urls.py
+++ django1_demo/django1_demo/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from . import views
+from . import views 
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('goodbye/', views.index1, name='index1'),
+    path('main/', views.index2, name='index2'),
 ]
--- django1_demo/django1_demo/base/views.py
+++ django1_demo/django1_demo/base/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. 0e5b1c1f is the polls index.")
+  template = loader.get_template('hello.html')
+  return HttpResponse(template.render())
+
+def index1(request):
+  template = loader.get_template('goodbye.html')
+  return HttpResponse(template.render())
+
+def index2(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
